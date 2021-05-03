ac16d6ad00d88d995d1c2ca973f6497035cb7f57
aaallliiis <alisolatni59@icloud.com>
2021-05-03 16:33:04 +0430

    some changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const auth = require('./modules/auth.js');
 
 const app = express()
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
