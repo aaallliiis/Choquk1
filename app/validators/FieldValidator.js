79962181727423305e459fe4335b68f938eec100
aaallliiis <alisolatni59@icloud.com>
2021-08-10 19:51:27 +0430

    change:field name length limit

--- app/validators/FieldValidator.js
+++ app/validators/FieldValidator.js
@@ -1,10 +1,13 @@
-const { check } = require('express-validator');
-const Validator =  require('./Validator');
+const { check } = require("express-validator");
+const Validator = require("./Validator");
 
-class FieldValidator extends Validator{
-  handle(){
+class FieldValidator extends Validator {
+  handle() {
     return [
-      check('name', 'نام نمیتواند کمتر از ۴ حرف باشد').trim().escape().isLength({min:4}),
+      check("name", "نام نمیتواند کمتر از ۲ حرف باشد")
+        .trim()
+        .escape()
+        .isLength({ min: 2 }),
     ];
   }
 }
