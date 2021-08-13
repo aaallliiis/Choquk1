38a8e3c69170dee7adea0259593426dca6817a24
aaallliiis <alisolatni59@icloud.com>
2021-08-13 13:53:46 +0430

    add:validator to profile controller

--- app/http/controllers/api/v1/user/profileController/ProfileController.js
+++ app/http/controllers/api/v1/user/profileController/ProfileController.js
@@ -1,28 +1,35 @@
-const controller = require('../../../../controller');
-const User=require('../../../../../../models/User');
+const controller = require("../../../../controller");
+const User = require("../../../../../../models/User");
 
 class ProfileController extends controller {
-    async getUserData(req,res,next){
-        return this.success(await User.getUserData(req.user.id),res)
+  async getUserData(req, res, next) {
+    return this.success(await User.getUserData(req.user.id), res);
+  }
+
+  async updateUser(req, res) {
+    const errors = this.validationData(req);
+    if (errors.length > 0) return this.failed(errors, res, 400);
+    const {
+      user: { id },
+      body,
+    } = req;
+    const { phoneNumber, email, uniCode } = body;
+    if (phoneNumber || email || uniCode) {
+      var found = await User.find({
+        $or: [{ email }, { phoneNumber }, { uniCode }],
+      });
+      if (found.length > 0 && found.find((item) => item.id !== id))
+        return this.failed(
+          "این ایمل یا شماره همراه یا کد دانشجویی قبلا ثبت شده است",
+          res
+        );
     }
+    delete body.active;
 
-    async updateUser({user:{id},body},res){
-        const {phoneNumber,email,uniCode,active} = body;
-        if(phoneNumber||email||uniCode){
-            var found = await User.find({$or: [
-                {email},
-                {phoneNumber},
-                {uniCode}
-            ]});
-            if(found.length>0&&found.find(item=>item.id!==id)) return this.failed('این ایمل یا شماره همراه یا کد دانشجویی قبلا ثبت شده است',res)
-        }
-        delete body.active
-
-        User.findByIdAndUpdate(id,{$set:body})
-        .then(()=>this.success('با موفقیت ویرایش شد',res))
-        .catch(err=>console.log(err))
-    }
-}   
-
+    User.findByIdAndUpdate(id, { $set: body })
+      .then(() => this.success("با موفقیت ویرایش شد", res))
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = new ProfileController();
--- app/router/api/v1/user/profile/index.js
+++ app/router/api/v1/user/profile/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 //cotroller
-const profileController = require('../../../../../http/controllers/api/v1/user/profileController/ProfileController');
+const profileController = require("../../../../../http/controllers/api/v1/user/profileController/ProfileController");
 
 //validator
+const ProfileValidator = require("../../../../../validators/ProfileValidator");
 
-router.get('/', profileController.getUserData)
-router.put('/', profileController.updateUser)
+router.get("/", profileController.getUserData);
+router.put("/", ProfileValidator.handle(), profileController.updateUser);
 
 module.exports = router;
