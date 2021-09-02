736b27f64c086087c772e0144b57419a356eb155
aaallliiis <alisolatni59@icloud.com>
2021-09-02 17:23:53 +0430

    debug:seed

--- seed/index.js
+++ seed/index.js
@@ -2,11 +2,13 @@ const Admin = require("../app/models/Admin");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
+global.config = require("../config");
 
 const createAdmin = async () => {
-  app.listen(8080);
+  const server = app.listen(config.port);
   mongoose.Promise = global.Promise;
-  await mongoose.connect("mongodb://localhost/choquk", {
+  await mongoose.connect(config.database.url, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -20,6 +22,8 @@ const createAdmin = async () => {
 
   await admin.save();
   console.log("admin created");
+  mongoose.disconnect();
+  server.close();
 };
 
 createAdmin();
